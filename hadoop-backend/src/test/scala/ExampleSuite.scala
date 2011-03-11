ed4a2324983b4263cb7a7e5b291d8579ee02d752
Vojin Jovanovic <vjovanovic@kung-fu-laptop.(none)>
2011-03-11 02:58:42 +0100

    Adding hadoop-backend module

---
+++ hadoop-backend/src/test/scala/ExampleSuite.scala
@@ -0,0 +1,39 @@
+import collection.mutable.ListBuffer
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.testng.TestNGSuite
+import org.testng.annotations.{Test, Configuration}
+
+/**
+ * User: vjovanovic
+ * Date: 3/11/11
+ */
+
+class ExampleSuite extends TestNGSuite with ShouldMatchers {
+
+  var sb: StringBuilder = _
+  var lb: ListBuffer[String] = _
+
+  @Configuration { val beforeTestMethod = true }
+  def initialize() {
+    sb = new StringBuilder("ScalaTest is ")
+    lb = new ListBuffer[String]
+  }
+
+  @Test def verifyEasy() { // Uses ScalaTest assertions
+    sb.append("easy!")
+    assert(sb.toString === "ScalaTest is easy!")
+    assert(lb.isEmpty)
+    lb += "sweet"
+    intercept[StringIndexOutOfBoundsException] {
+      "concise".charAt(-1)
+    }
+  }
+
+  @Test def verifyFun() { // Uses ScalaTest matchers
+    sb.append("fun!")
+    sb.toString should be ("ScalaTest is fun!")
+    lb should be ('empty)
+    lb += "sweet"
+    evaluating { "concise".charAt(-1) } should produce [StringIndexOutOfBoundsException]
+  }
+}
