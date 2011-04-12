70a0f3451c8b2815bb1aedf4afe709133d255dca
Vojin Jovanovic <vjovanovic@kung-fu-laptop.(none)>
2011-04-12 21:24:21 +0200

    Code cleanup in progress...

--- distributed-collections/src/main/scala/examples/SetExample.scala
+++ distributed-collections/src/main/scala/examples/SetExample.scala
@@ -13,14 +13,14 @@ object SetExample {
   def main(args: Array[String]) {
     val distributedSet = new DistSet[Long](new URI("long-set"))
     val ds2 = new DistSet[Long](new DistSet[Long](new URI("long-set1")).filter(_ > 50).location)
-    val ds2000 = new DistSet[Long](new DistSet[Long](new URI("long-set1")).flatMap((v: Long) => for (i <- 0 to 20) yield (v + i)  * 20L).location)
+    val ds2000 = new DistSet[Long](new DistSet[Long](new URI("long-set1")).flatMap((v: Long) => for (i <- 0 to 20) yield (v + i) * 20L).location)
     val messages = ArrayBuffer[String]()
 
-    messages += "Intersection of (0 to 99) and (50 to 99) =" + (distributedSet.intersect(ds2).toString())
-    messages += "Diff of (0 to 99) and (50 to 99) =" + (distributedSet.diff(ds2).toString())
-    messages += "(0 to 99) + 12345 contains 12345 =" + ((distributedSet + 12345).contains(12345).toString())
-    messages += "(0 to 99) + 12345 contains 12346 =" + ((distributedSet + 12345).contains(12346).toString())
-    messages += "(0 to 99) union (0 to 2000) =" + ((distributedSet + 12345).contains(12346).toString())
+    messages += "Intersection of (0 to 99) and (50 to 99) =" + (distributedSet intersect ds2 toString())
+    messages += "Diff of (0 to 99) and (50 to 99) =" + (distributedSet diff ds2 toString())
+    messages += "(0 to 99) + 12345 contains 12345 =" + (distributedSet + 12345 contains 12345)
+    messages += "(0 to 99) + 12345 contains 12346 =" + (distributedSet + 12345 contains 12346)
+    messages += "(0 to 99) union (0 to 2000) =" + (distributedSet union ds2000 toString())
 
 
     messages.foreach(println)
